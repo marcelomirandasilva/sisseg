e592876317505f491120df741454187b582a85e8
marcelomirandasilva <marcelo.miranda.pp@gmail.com>
2019-05-20 15:01:39 -0300

    checkpoint

--- app/Http/Controllers/Api/FuncionarioController.php
+++ app/Http/Controllers/Api/FuncionarioController.php
@@ -5,43 +5,76 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Funcionario;
+use App\API\ApiError;
 
 class FuncionarioController extends Controller
 {
-    public function index()
-    {
-        $funcionarios = Funcionario::all();
+	public function __construction(Funcionario $funcionario)
+	{
+		$this->funcionario = $funcionario;
+	}
 
-        return $funcionarios;
-    }
-    
-    public function create()
-    {
-        //
-    }
+	public function index()
+	{
+		$funcionarios = Funcionario::all();
+		return response()->json(['data' => $funcionarios]); 
+	}
+	
+	public function create()
+	{
+		//
+	}
 
-    public function store(Request $request)
-    {
-        //
-    }
+	public function store(Request $request)
+	{
+		try {
+			$funcionarioData = $request->all();
+			Funcionario::create($funcionarioData);
 
-    public function show($id)
-    {
-        //
-    }
+			$retorno = ['data' => ['msg' => 'Funcionario criado!']];
+			return response()->json($retorno, 201); 
 
-    public function edit($id)
-    {
-        //
-    }
+		} catch (\Exception $e) {
+			if (config('app.debug')) {
+				return response()->json(ApiError::errorMessage($e->getMessage(), 1010)); 
+			}
 
-    public function update(Request $request, $id)
-    {
-        //
-    }
+			return response()->json(ApiError::errorMessage("Erro ao executar operação", 1010)); 
+		}
+	}
 
-    public function destroy($id)
-    {
-        //
-    }
+	public function show( $id)
+	{
+		$data = Funcionario::find($id);
+		return response()->json(['data' => $data]); 
+	}
+
+	public function edit($id)
+	{
+		//
+	}
+
+	public function update(Request $request, $id)
+	{
+		try {
+			$funcionario = Funcionario::find($id);
+			$funcionario->update($request->all());
+
+			$retorno = ['data' => ['msg' => 'Funcionario Alterado!']];
+			return response()->json($retorno, 201); 
+
+		} catch (\Exception $e) {
+			if (config('app.debug')) {
+				return response()->json(ApiError::errorMessage($e->getMessage(), 1010)); 
+			}
+
+			return response()->json(ApiError::errorMessage("Erro ao executar operação", 1010)); 
+		}
+
+	}
+
+	public function destroy($id)
+	{
+		//
+	}
 }
--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,6 @@ use Illuminate\Http\Request;
 //     return $request->user();
 // });
 
-
-
 // Route::group(['middleware' => 'auth:api'], function(){
 //     Route::apiResource('/funcionarios', "Api\FuncionarioController");
 //     Route::get("/setoresporsecretaria/{id}", "SetorController@setoresporsecretaria");
@@ -30,19 +28,17 @@ use Illuminate\Http\Request;
 
 
 /// rota para buscar setores de uma determinada secretaria
-    Route::get("/setoresporsecretaria/{id}", "Api\SetorController@setoresporsecretaria");
-    /// rota para buscar cargos de um determinado setor
-    Route::get("/cargosporsecretaria/{id}", "Api\CargoController@cargosporsecretaria");
-    Route::apiResource('/funcionarios', "Api\FuncionarioController");
-
-
+Route::get("/setoresporsecretaria/{id}", 				"Api\SetorController@setoresporsecretaria");
+/// rota para buscar cargos de um determinado setor
+Route::get("/cargosporsecretaria/{id}", 				"Api\CargoController@cargosporsecretaria");
 
 /// rota para verificar se o NOME DO SISTEMA já está cadastrado
-Route::get("/verificaSistemaExiste/{sistema}", "Api\SistemaController@verificaSistemaExiste");
+Route::get("/verificaSistemaExiste/{sistema}", 		"Api\SistemaController@verificaSistemaExiste");
 
 /// rota para verificar se o NOME DA ROLE já existe em determinado SISTEMA
 Route::get("/verificaRoleExiste/{sistema}/{role}", "Api\RoleController@verificaRoleExiste");
 
+Route::post("/verificaRoleFuncionario", 				"Api\RoleController@verificaRoleFuncionario");
 
-Route::post("/verificaRoleFuncionario", "Api\RoleController@verificaRoleFuncionario");
-
+//resource
+Route::apiResource('/funcionario', 						"Api\FuncionarioController");
