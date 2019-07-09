5e8cc0111de71171aeedb17fd88d9afa7fb8d033
marcelomirandasilva <marcelo.miranda.pp@gmail.com>
2019-07-09 10:29:18 -0300

    teste

--- app/Http/Controllers/FuncionarioController.php
+++ app/Http/Controllers/FuncionarioController.php
@@ -55,7 +55,7 @@ class FuncionarioController extends Controller
 	}
 	public function store(Request $request)
 	{
-		//dd($request->all());
+		dd($request->all());
 		
 		// Validar os campos
 		$this->validate($request, [
