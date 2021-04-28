bcdc57de96e73cb551e5007278a882cdb6508e3b
Marcelo <marcelo.miranda.pp@gmail.com>
2021-04-28 11:45:31 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,10 +11,12 @@ git pull
 #php artisan route:cache
 
 cp -R /var/www/fw_sisseg/public /var/www/html/sisseg/
+cp -R /var/www/fw_sisseg/public/build /var/www/html/sisseg/
 cp -R /var/www/fw_sisseg/public/css /var/www/html/sisseg/
-cp -R /var/www/fw_sisseg/public/fonts /var/www/html/sisseg/
-cp -R /var/www/fw_sisseg/public/img /var/www/html/sisseg/
+cp -R /var/www/fw_sisseg/public/datatables /var/www/html/sisseg/
+cp -R /var/www/fw_sisseg/public/images /var/www/html/sisseg/
 cp -R /var/www/fw_sisseg/public/js /var/www/html/sisseg/
+cp -R /var/www/fw_sisseg/public/vendors /var/www/html/sisseg/
 
 #/etc/init.d/apache2 restart
 
